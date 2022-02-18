7d7b606442555b68153996b76bd71c0b02ffe61c
Edward Burgos <80774061+edwardburgos@users.noreply.github.com>
2022-02-17 23:27:18 -0500

    :lipstick: landscape mode updated in ExtendedPhoto component

--- app/src/main/java/com/example/nasaapp/components/ExtendedPhoto.kt
+++ app/src/main/java/com/example/nasaapp/components/ExtendedPhoto.kt
@@ -1,12 +1,15 @@
 package com.example.nasaapp.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -15,21 +18,55 @@ import com.example.domain.Photo
 
 @Composable
 fun ExtendedPhoto(photo: Photo) {
-    Column {
-        Image(
-            painter = rememberImagePainter(data = photo.img_src),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-        Column(
-            modifier = Modifier.padding(32.dp).padding(bottom = 0.dp)
-        ) {
-            Text("Photo Description", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = MaterialTheme.colors.secondary, modifier = Modifier.padding(bottom = 8.dp))
-            Text("Taken by ${photo.rover.name} Mars Rover in its ${photo.camera.full_name} ", color = MaterialTheme.colors.secondary, lineHeight = 20.sp)
-            Text("Taken in sol number ${photo.sol}", color = MaterialTheme.colors.secondary, lineHeight = 20.sp)
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            Row {
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = rememberImagePainter(data = photo.img_src),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(start = 32.dp)
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text("Photo Description", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = MaterialTheme.colors.secondary, modifier = Modifier.padding(bottom = 8.dp))
+                    Text("Taken by ${photo.rover.name} Mars Rover in its ${photo.camera.full_name} ", color = MaterialTheme.colors.secondary, lineHeight = 20.sp)
+                    Text("Taken in sol number ${photo.sol}", color = MaterialTheme.colors.secondary, lineHeight = 20.sp)
+                }
+            }
         }
+        else -> {
+            Column {
+                Image(
+                    painter = rememberImagePainter(data = photo.img_src),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(32.dp)
+                        .padding(bottom = 0.dp)
+                ) {
+                    Text("Photo Description", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = MaterialTheme.colors.secondary, modifier = Modifier.padding(bottom = 8.dp))
+                    Text("Taken by ${photo.rover.name} Mars Rover in its ${photo.camera.full_name} ", color = MaterialTheme.colors.secondary, lineHeight = 20.sp)
+                    Text("Taken in sol number ${photo.sol}", color = MaterialTheme.colors.secondary, lineHeight = 20.sp)
+                }
 
+            }
+        }
     }
 }
