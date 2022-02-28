ebbc8d464650c337e69ab1c5f19c27ccf2e2e3ae
Edward Burgos <80774061+edwardburgos@users.noreply.github.com>
2022-02-28 12:50:03 -0500

    :art: retrofit2.Response implemented

--- data/src/main/java/com/example/data/network/ApiService.kt
+++ data/src/main/java/com/example/data/network/ApiService.kt
@@ -1,23 +1,17 @@
 package com.example.data.network
 
 import com.example.data.network.model.ResponseApi
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-//TODO: Retrofit has built-in support for suspend functions, which would integrate seamlessly with your code considering
-// you're already using coroutines and flow. It also has a response wrapper with various utilities.
-// Instead of having Call<ResponseApi> it could be Response<ResponseApi>.
-/**
-* @see retrofit2.Response
-* */
 interface ApiService {
     @GET("curiosity/photos")
-    fun getPhotosCuriosity(@Query("sol") sol: String, @Query("camera") selectedCamera: String?): Call<ResponseApi>
+    suspend fun getPhotosCuriosity(@Query("sol") sol: String, @Query("camera") selectedCamera: String?): Response<ResponseApi>
 
     @GET("opportunity/photos")
-    fun getPhotosOpportunity(@Query("sol") sol: String, @Query("camera") selectedCamera: String?): Call<ResponseApi>
+    suspend fun getPhotosOpportunity(@Query("sol") sol: String, @Query("camera") selectedCamera: String?): Response<ResponseApi>
 
     @GET("spirit/photos")
-    fun getPhotosSpirit(@Query("sol") sol: String, @Query("camera") selectedCamera: String?): Call<ResponseApi>
+    suspend fun getPhotosSpirit(@Query("sol") sol: String, @Query("camera") selectedCamera: String?): Response<ResponseApi>
 }
--- data/src/main/java/com/example/data/repository/PhotoRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/PhotoRepositoryImpl.kt
@@ -3,15 +3,11 @@ package com.example.data.repository
 import android.util.Log
 import com.example.data.network.ApiService
 import com.example.data.network.model.PhotoApiMapper
-import com.example.data.network.model.ResponseApi
 import com.example.data.network.model.ResponseState
 import com.example.data.repository.model.GetPhotosResponse
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import javax.inject.Inject
 
 class PhotoRepositoryImpl @Inject constructor(
@@ -25,33 +21,30 @@ class PhotoRepositoryImpl @Inject constructor(
     override fun getPhotosFromApi(roverName: String, sol: String, selectedCamera: String): Flow<GetPhotosResponse> {
         val camera = if (selectedCamera == "All") null else selectedCamera.lowercase()
         return flow {
-            val source = when (roverName.lowercase()) {
-                "curiosity" -> apiService.getPhotosCuriosity(sol, camera)
-                "opportunity" -> apiService.getPhotosOpportunity(sol, camera)
-                else -> apiService.getPhotosSpirit(sol, camera)
-            }
             var finalResponse = GetPhotosResponse(ResponseState.INITIAL, listOf())
-
-            // TODO: This is where using Retrofit's suspend function support would come in handy.
-            //  You would have the ability to write asynchronous code in a synchronous way without having
-            //  to call source.enqueue nor overriding onResponse and onFailure. It would make the overall code
-            //  in the repository implementation cleaner.
-            source.enqueue(object : Callback<ResponseApi> {
-                override fun onResponse(call: Call<ResponseApi>, response: Response<ResponseApi>) {
-                    if (response.isSuccessful) {
-                        response.body()?.let {
-                            finalResponse = GetPhotosResponse(if (it.photos.isEmpty()) ResponseState.EMPTY else ResponseState.FILLED, apiMapper.fromEntityList(it.photos))
-                        }
+            val errorResponse = GetPhotosResponse(ResponseState.ERROR, listOf())
+            while (true) {
+                try {
+                    val source = when (roverName.lowercase()) {
+                        "curiosity" -> apiService.getPhotosCuriosity(sol, camera)
+                        "opportunity" -> apiService.getPhotosOpportunity(sol, camera)
+                        else -> apiService.getPhotosSpirit(sol, camera)
                     }
-                }
-                override fun onFailure(call: Call<ResponseApi>, t: Throwable) {
-                    Log.e("Failure", "Message " + t.message)
+                    if (source.isSuccessful) {
+                        source.body()?.let {
+                            finalResponse = GetPhotosResponse(if (it.photos.isEmpty()) ResponseState.EMPTY else ResponseState.FILLED, apiMapper.fromEntityList(it.photos))
+                        } ?: run {
+                            finalResponse = errorResponse
+                        }
+                    } else {
+                        finalResponse = errorResponse
+                    }
+                } catch (e: Exception) {
+                    Log.e("Failure", "Message " + e.message)
                     //TODO: Comparing against a specific error message isn't a reliable way to check whether or not the phone has internet access.
                     //  Using something like ConnectivityManager would be a better approach.
-                    finalResponse = GetPhotosResponse(if (t.message == "Unable to resolve host \"api.nasa.gov\": No address associated with hostname") ResponseState.NO_INTERNET else ResponseState.ERROR, listOf())
+                    finalResponse = GetPhotosResponse(if (e.message == "Unable to resolve host \"api.nasa.gov\": No address associated with hostname") ResponseState.NO_INTERNET else ResponseState.ERROR, listOf())
                 }
-            })
-            while (true) {
                 emit(finalResponse)
                     if (finalResponse.status == ResponseState.INITIAL) {
                         delay(refreshIntervalMsShort)
